388772313d794a12d521b07c25519cc6f0454c96
Felipe Alves de Sousa <103237188+ffelipealves@users.noreply.github.com>
2022-04-27 09:38:39 -0300

    questao 13

---
+++ exercicios/lista 4/questao13/questao13.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/*
+13) Faça um programa para calcular a transposta de uma matriz 5 x 4. Imprima as duas matrizes na
+tela
+*/
+
+int main(){
+
+    int matriz[5][4]={32, 39, 50, 5, 22, 14, 29, 24, 5, 8, 27, 27, 16, 48, 16, 22, 28, 16, 6, 37};
+    int matrizTransposta[4][5]={0},i,j;
+
+    //INVERTEMOS OS INDICES DA MATRIZ TRANSPOSTA PARA PREENCHELA
+    for(i=0;i<5;i++){
+        for(j=0;j<4;j++){
+            if(i==4 && j==4){
+                i++;
+            }else{
+                matrizTransposta[j][i]=matriz[i][j];
+            }
+
+
+        }
+    }
+    //PRINTANDO MATRIZ ORIGIGNAL
+    printf("MATRIZ ORIGINAL: \n");
+    for(i=0;i<5;i++){
+        for(j=0;j<4;j++){
+            printf("%3d ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    //PRINTANDO MATRIZ TRANSPOSTA
+    printf("MATRIZ TRANSPOSTA: \n");
+    for(i=0;i<4;i++){
+        for(j=0;j<5;j++){
+            printf("%3d ",matrizTransposta[i][j]);
+        }
+        printf("\n");
+    }
+}
