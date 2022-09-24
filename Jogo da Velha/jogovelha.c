905b01a766dd94289860932f9ab5e1e6fc4bf5cb
Felipe Alves <103237188+ffelipealves@users.noreply.github.com>
2022-09-24 11:40:38 -0300

    exe

---
+++ Jogo da Velha/jogovelha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+void mostraTabuleiro(){
+
+    printf("\t#####JOGO DA VELHA#####\n\n\t       A | B | C \n\t      ___|___|___");
+
+}
+
+int main(){
+
+
+    mostraTabuleiro();
+
+    return 0;
+}
