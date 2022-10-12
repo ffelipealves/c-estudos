f57feabda06d34678a68de4970144b583c11f330
Felipe Alves <103237188+ffelipealves@users.noreply.github.com>
2022-10-12 16:49:17 -0300

    in construction

--- Jogo da Velha/jogovelha.c
+++ Jogo da Velha/jogovelha.c
@@ -1,16 +1,39 @@
 #include<stdio.h>
 
+char criaTabuleiro(){
+
+    char jogoVelha[3][3]= {'a','b','c','d','e','f','g','h','i'};
+    return jogoVelha;
+}
 
 void mostraTabuleiro(){
 
-    printf("\t#####JOGO DA VELHA#####\n\n\t       A | B | C \n\t      ___|___|___");
+    printf("\n\t#JOGO DA VELHA#\n\n\t  X | O | X\n\t  -   -   -\n\t  O | X | O\n\t  -   -   -\n\t  X | O | X\n\n");
 
+}
+
+void jogada(){
+
+    char posicao;
+    printf("\n\nPLAYER  -Digite uma posicao disponivel acima: ");
+    scanf(" %c",&posicao);
+}
+
+char teste(){
+    char jogoVelha[3][3]= {'a','b','c','d','e','f','g','h','i'};
+    return jogoVelha;
+}
+
+void teste2(char array){
+
+    printf("%c",array[0][0]);
 }
 
 int main(){
 
 
-    mostraTabuleiro();
+    teste2(teste());
+
 
     return 0;
 }
